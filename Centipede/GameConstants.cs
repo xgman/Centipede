8d3c14da164e938add4d815f69f630f6beb6f6f4
xgman <axel.wreeby@gmail.com>
2018-04-22 21:41:30 +0200

    .

--- Centipede/GameConstants.cs
+++ Centipede/GameConstants.cs
@@ -11,7 +11,7 @@ namespace Centipede
         public const int WindowWidth = 720;
         public const int WindowHeight = 720;
 
-        public const int PlayerClampHeight = GameConstants.WindowHeight - 24 * 7;
+        public const int PlayerClampHeight = WindowHeight - 24 * 7;
         public const float PlayerBulletSpeed = 0.4f;
     }
 }
